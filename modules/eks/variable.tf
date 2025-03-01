4a7b6d8e30387dc569f90425383ffd250f2b5bf9
Ernesto Deluquez Heilbron <edeluquez@hotmail.com>
2025-03-01 08:14:28 +0000

    ✨Initial commit

---
+++ modules/eks/variable.tf
@@ -0,0 +1,10 @@
+
+variable "vpc_id" {}
+variable "domain" {}
+variable "zone_id" {}
+variable "max_nodes" {}
+variable "min_nodes" {}
+variable "desired_nodes" {}
+variable "cluster_version" {}
+variable "admin_allowed_ips" {}
+variable "private_subnet_ids" {}
---
+++ variables.tf
@@ -0,0 +1,46 @@
+
+variable "environment" { #REQUIRED
+  description = "Environment Name (dev, qa, staging, prod)"
+  type        = string
+  default     = "dev"
+
+  validation {
+    condition     = can(regex("^(dev|aq|stag|pr[do])[a-z0-9]{0,9}$", var.environment))
+    error_message = "The environment name must start with 'dev', 'pro', or 'prd', followed by up to 9 lowercase letters or numbers, with a total length between 3 and 12 characters."
+  }
+
+  validation {
+    condition     = terraform.workspace == var.environment
+    error_message = "Invalid workspace: The active workspace '${terraform.workspace}' does not match the specified environment '${var.environment}'."
+  }
+}
+
+## EKS
+variable "admin_allowed_ips" {
+  type    = string
+  default = ""
+}
+
+variable "cluster_version" {
+  description = "Cluster version"
+  type        = string
+  default     = "1.32"
+}
+
+variable "max_nodes" {
+  description = "Maximun nodes at cluster"
+  type        = number
+  default     = 2
+}
+
+variable "min_nodes" {
+  description = "Mininum nodes at cluster"
+  type        = number
+  default     = 2
+}
+
+variable "desired_nodes" {
+  description = "Desired nodes at cluster"
+  type        = number
+  default     = 2
+}
